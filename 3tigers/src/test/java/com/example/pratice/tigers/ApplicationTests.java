18274a434145fdb3b5260c54e227ec805c776efa
sumanbazar123 <124317019+sumanbazar123@users.noreply.github.com>
2023-06-07 13:55:01 -0500

    Initial commit

---
+++ 3tigers/src/test/java/com/example/pratice/tigers/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pratice.tigers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
