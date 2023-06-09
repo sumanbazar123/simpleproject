bffbaf7191c17006f09cb9b53f3ca8b2dad89a11
sumanbazar123 <124317019+sumanbazar123@users.noreply.github.com>
2023-06-09 11:18:21 -0500

    Tasks Package

---
+++ 3tigers/src/main/java/com/example/pratice/tigers/tasks/task1.java
@@ -0,0 +1,11 @@
+package com.example.pratice.tigers.tasks;
+
+public class task1
+{
+    //Topics : Data Types
+    //Everyone should create a package as "Tasks" ,create a class in that and practice programs in that class
+    //Work on the data types
+    // write program to find the MIN_VALUE AND MAX_VALUE of each data type
+
+
+}
