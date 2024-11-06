ac83d3e600d872474867cbb11a019e6537ec5b2f
sumanbazar123 <124317019+sumanbazar123@users.noreply.github.com>
2024-11-06 15:14:35 -0600

    New Test Class added Commit2

--- 3tigers/src/main/java/com/example/pratice/tigers/tasks/NewTest.java
+++ 3tigers/src/main/java/com/example/pratice/tigers/tasks/NewTest.java
@@ -4,7 +4,8 @@ public class NewTest
 {
     public static void main(String[] args)
     {
+        System.out.println("Hi all");
         System.out.println("Hi, How are you all");
-        
+
     }
 }
