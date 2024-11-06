ab90db556baa08ce6c4bca1f6d9833b718ffba32
sumanbazar123 <124317019+sumanbazar123@users.noreply.github.com>
2024-11-06 14:43:00 -0600

    Added main method

--- 3tigers/src/main/java/com/example/pratice/tigers/tasks/TigerTest.java
+++ 3tigers/src/main/java/com/example/pratice/tigers/tasks/TigerTest.java
@@ -1,4 +1,13 @@
 package com.example.pratice.tigers.tasks;
 
+<<<<<<< HEAD
 public class TigerTest {
 }
+=======
+public class TigerTest
+{
+        public static void main(String args[]) {
+    System.out.println("Tiger Class");
+}
+}
+>>>>>>> bb277b9 (Added main method)
