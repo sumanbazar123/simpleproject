e5d06417c537d3d47b7188a80f7c0e0f74e88752
sumanbazar123 <124317019+sumanbazar123@users.noreply.github.com>
2024-11-06 15:05:26 -0600

    Added some hi Command

--- 3tigers/src/main/java/com/example/pratice/tigers/tasks/SampleTest.java
+++ 3tigers/src/main/java/com/example/pratice/tigers/tasks/SampleTest.java
@@ -1,4 +1,9 @@
 package com.example.pratice.tigers.tasks;
 
-public class SampleTest {
+public class SampleTest
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hi");
+    }
 }
