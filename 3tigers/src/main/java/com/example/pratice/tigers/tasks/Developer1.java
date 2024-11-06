cc7d3c45bdaf286ff04c79aaabdc346207c7d970
sumanbazar123 <124317019+sumanbazar123@users.noreply.github.com>
2024-11-06 16:20:22 -0600

    Dev1 commit

---
+++ 3tigers/src/main/java/com/example/pratice/tigers/tasks/Developer1.java
@@ -0,0 +1,14 @@
+package com.example.pratice.tigers.tasks;
+
+public class Developer1
+{
+    public static void main(String[] args)
+    {
+            System.out.println("Developer 1 added");
+        }
+        static void Dev1() {
+            System.out.println("Dev1");
+        }
+
+}
+
